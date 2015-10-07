3c26a7b39707e7f8fe1ecbf34441ff1a1041661f
ckychris <ezreal223@gmail.com>
2015-10-07 20:22:08 +1100

    login CSS

--- app/views/login-view.php
+++ app/views/login-view.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
    <head>
-      <title>Silverado - Index page</title>
+      <title>Silverado - Login page</title>
       <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1, maximum-scale=1">
       <link href='https://fonts.googleapis.com/css?family=Poiret+One' rel='stylesheet' type='text/css'>
       <style>
@@ -9,7 +9,6 @@
             <?php require_once('CSS/login.css'); ?>
       </style>
       <script src="app/views/JS/jquery-2.1.4.min.js"></script>
-      <script src="app/views/JS/login.js"></script>
    </head>
    <body>
       <div id="warpper">
@@ -22,7 +21,7 @@
                     <legend class="book">Login</legend>
                         Email:<input type="email" name="email" value="" required></input>
                         Password:<input type="text" name="token" value="" required></input>
-                        <input type="submit" value="Submit">
+                        <input type="submit" value="Submit"/>
                 </fieldset>
             </form>
          </div>
